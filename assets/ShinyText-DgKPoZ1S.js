f4851b34d149a7092a2b456f401a515f531ad074
DieOsorio <dieosorio@outlook.com>
2025-07-09 18:18:22 +0000

    Deploying to deploy from @ DieOsorio/TuneBridge@ae7d6b599bb283f88a8c99ac2981970033b06797 🚀

--- assets/ShinyText-DgKPoZ1S.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./mui-B-xP7mtj.js";const x=({text:s,disabled:t=!1,speed:e=5,className:i="",style:n={}})=>{const a=`${e}s`;return o.jsx("div",{className:`shiny-text ${t?"disabled":""} ${i}`,style:{animationDuration:a,...n},children:s})};export{x as S};
